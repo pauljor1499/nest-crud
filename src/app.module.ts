d9c7e20aa0906a3ed03062398dade6bcc9912809
paulj <jorpaulvincent@gmail.com>
2024-03-03 11:14:42 +0800

    update db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [],
+	imports: [
+		MongooseModule.forRoot("mongodb+srv://admin:uaqw25c6bsqDciyq@cluster0.aeltnpt.mongodb.net/AngularTodo?retryWrites=true&w=majority&appName=Cluster0"),
+		BookModule
+	],
 	controllers: [AppController],
 	providers: [AppService,],
 })
