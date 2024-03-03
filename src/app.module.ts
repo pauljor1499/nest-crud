fc567c64f552b69b9882a17dcfbe291439fc790e
paulj <jorpaulvincent@gmail.com>
2024-03-03 11:41:26 +0800

    remove books instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BookModule } from './book/book.module';
 import { MongooseModule } from '@nestjs/mongoose';
+
+const DB_USER = 'admin';
+const DB_PASSWORD = 'uaqw25c6bsqDciyq';
+const DB_TABLE = 'NestCRUD'
 
 @Module({
 	imports: [
-		MongooseModule.forRoot("mongodb+srv://admin:uaqw25c6bsqDciyq@cluster0.aeltnpt.mongodb.net/AngularTodo?retryWrites=true&w=majority&appName=Cluster0"),
-		BookModule
+		MongooseModule.forRoot(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.aeltnpt.mongodb.net/${DB_TABLE}?retryWrites=true&w=majority&appName=Cluster0`),
 	],
 	controllers: [AppController],
 	providers: [AppService,],
